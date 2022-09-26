13da82cc0c51e4a5585e27185c4635c35e3411ca
Rami Riekkinen <rami.riekkinen@student.lab.fi>
2022-09-26 13:03:07 +0300

    Database connection, evrything works as they should now

--- src/server.js
+++ src/server.js
@@ -1,42 +1,92 @@
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql');
 const axios = require('axios');
-const app = express()
+const app = express();
+var schedule = require('node-schedule');
 
 app.set('view engine', 'ejs');
 
-// TODO card database
-// TODO my cards database ---> same as above, but with number owned prop 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "",
+    password: "",
+    database: ""
+});
 
-// endpoint: http://localhost:3001/all-cards
-app.get('/all-cards', async (req, res, body) => {
-    let cards = [];
-    let api;
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
-    // Get the API data
+let cards = [];
+let api;
+let apidata;
+
+// Get the API data
+const getApi = () => {
+    return new Promise(resolve => {
+        api = axios.get('https://api.scryfall.com/cards/search?q=e%3Admu')
+        //console.log(api)
+        resolve(api);
+    });
+};
+
+async function getCards(){
+    apidata = await getApi();
+    //console.log(apidata.data.data)
+
+    // Sort the data
+    // apidata.data.data.sort(function(a, b) { 
+    //     return a.collector_number - b.collector_number
+    // });
+
+    let sql = "REPLACE INTO allcards (id, name, collectionnumber, rarity, imageuri) VALUES ?";
+
+    //Arrange the API data to array
+    for (const [key, value] of Object.entries(apidata.data.data)) {  
+        cards.push([value.id, value.name, value.collector_number, value.rarity, value.image_uris.small])
+    }
+
+    con.query(sql, [cards], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+
+}
+
+getCards();
+
+// Update the database every minute
+// TODO when no longer needed -> minute to every 12 hours or so
+var scheduler = schedule.scheduleJob('*/1 * * * *', async function(){
     const getApi = () => {
         return new Promise(resolve => {
             api = axios.get('https://api.scryfall.com/cards/search?q=e%3Admu')
             resolve(api);
         });
     };
+    apidata = await getApi();
 
-apidata = await getApi();
-
-// Sort the data
-apidata.data.data.sort(function(a, b) { 
-    return a.collector_number - b.collector_number
+    getCards();
 });
 
-// Arrange the API data to array
-for (const [key, value] of Object.entries(apidata.data.data)) {  
-    cards.push([value.name, value.collector_number, value.rarity, value.image_uris.small])
-}
-
-
-console.log(cards)
-
-res.render('allcards.ejs', {cards: cards });
+// -------------------------- endpoints ---------------------------------
+//
+// endpoint: http://localhost:3001/all-cards
+app.get('/all-cards', async (req, res, body) => {
+    let allcards = [];
+    con.query("SELECT name, collectionnumber, rarity, imageuri FROM allcards", function (err, result, fields) {
+        if (err) throw err;
+        for (const [key, value] of Object.entries(result)) {  
+            allcards.push([value.name, value.collectionnumber, value.rarity, value.imageuri])
+        }
+        //console.log(allcards[1]);
+        res.render('allcards.ejs', {cards: allcards });
+    });
 });
+//
+// -------------------------- /endpoints ---------------------------------
+
 
 const PORT = 3001
 app.listen(PORT, () => {
