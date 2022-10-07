f217a6ac7d57b86d6e48b18c4013258074002214
ramiriekki <rami.riekkinen@gmail.com>
2022-10-07 20:55:57 +0300

    css styling moved to own folder and files to improve readability

--- src/server.js
+++ src/server.js
@@ -145,7 +145,8 @@ const app = express();
 // --------- middleware ---------
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.use('/public', express.static('public')); // access css files
 // --------- /middleware --------
 
 app.set('view engine', 'ejs');
