a86d348e8b3c41ec8a1339676f69166a2c43f8b7
ramiriekki <rami.riekkinen@gmail.com>
2022-11-03 20:24:38 +0200

    Styling

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardsComponent } from './cards/cards.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'cards', component: CardsComponent}
+  {path: 'cards', component: CardsComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
