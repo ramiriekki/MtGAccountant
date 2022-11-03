c16d0c62d1b3148fccf4c8997df31479331a78f6
ramiriekki <rami.riekkinen@gmail.com>
2022-11-03 11:11:45 +0200

    server service started

--- client/src/app/server.service.ts
+++ client/src/app/server.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
+  private cardsUrl = 'http://localhost:3001/collections/my-collection'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
 }
