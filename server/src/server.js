a121513b9a34d63edd7208664ec120e79418f130
ramiriekki <rami.riekkinen@gmail.com>
2022-11-09 09:40:17 +0200

    session start

--- server/src/server.js
+++ server/src/server.js
@@ -5,25 +5,41 @@ const app = express();
 const db = require("./db");
 const con = db.getCon()
 const schedule = require('node-schedule');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const mysqlStore = require('express-mysql-session')(sessions);
+
+// mysqlStore options
+const options ={
+    connectionLimit: 10,
+    password: process.env.ADMIN_PASSWORD,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    createDatabaseTable: true
+}
+
+const  sessionStore = new mysqlStore(options);
+
+app.use(sessions({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized:true,
+    store: sessionStore,
+    cookie: { /*httpOnly: true, secure: true, */ maxAge: 1000 * 30 },
+    resave: false
+}));
+
+app.use(cookieParser());
 
 app.use('/collections', collections)
+
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
 });
 
