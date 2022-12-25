2e8b86cefc3cd7c77e252da6bb6d58d91b1a0930
ramiriekki <rami.riekkinen@gmail.com>
2022-12-25 23:54:57 +0200

    JWT token

---
+++ server/src/main/java/com/github/mtgaccountant/server/jwt/JwtFilter.java
@@ -0,0 +1,70 @@
+package com.github.mtgaccountant.server.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CustomerUserDetailsService service;
+
+    Claims claims = null;
+    private String userName = null;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws IOException, ServletException{
+        if (httpServletRequest.getServletPath().matches("/user/login | user/forgot-password")){
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        } else {
+            String authorizationHeader = httpServletRequest.getHeader("Authorization");
+            String token = null; 
+
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                token = authorizationHeader.substring(7);
+                userName = jwtUtil.extractUsername(token);
+                claims = jwtUtil.extractAllClaims(token);
+            }
+
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = service.loadUserByUsername(userName);
+                if(jwtUtil.validateToken(token, userDetails)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
+
+    public boolean isAdmin() {
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public boolean isUser() {
+        return "user".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public String getCurrentUser(){
+        return userName;
+    }
+    
+}
