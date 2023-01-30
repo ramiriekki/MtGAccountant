04475b52c74ed35e0fd1a9f43c777e37bea689e6
Rami Riekkinen <rami.riekkinen@gmail.com>
2023-01-30 10:53:26 +0200

    Modified card class so that double faced cards get correct data

--- server/src/main/java/com/github/mtgaccountant/server/models/Card.java
+++ server/src/main/java/com/github/mtgaccountant/server/models/Card.java
@@ -12,43 +12,25 @@ import lombok.Data;
 @Document("cards")
 public class Card implements Serializable{
     private String id;
-    
     private String name;
-
     private String released_at;
-
     private ImageUri image_uris;
-
     private String oracle_text;
-
     private String power;
-
     private String toughness;
-
     private String[] colors;
-
     private String[] color_identity;    
-
     private String[] keywords;
-
+    private DoubleFaceCard[] card_faces;
     @JsonProperty("set")
     private String set_code;
-
     private String set_name;
-
     private String set_type;
-
     private String collector_number;
-
     private String rarity;
-
     private String flavor_text;
-
     private String artist;
-
     private Prices prices;
-
     private PurchaseUri purchase_uris;
-
     private boolean collected;
 }
--- server/src/main/java/com/github/mtgaccountant/server/rest/SearchRest.java
+++ server/src/main/java/com/github/mtgaccountant/server/rest/SearchRest.java
@@ -1,14 +1,11 @@
 package com.github.mtgaccountant.server.rest;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.github.mtgaccountant.server.models.ClientSearch;
 import com.github.mtgaccountant.server.wrapper.CardSearchWrapper;
