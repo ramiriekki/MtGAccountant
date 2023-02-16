04c087ca3f9b70338565ed34579146e825f72e67
Rami Riekkinen <rami.riekkinen@gmail.com>
2023-02-16 14:52:00 +0200

    Better support for 2 face cards

--- client/src/app/card/card.component.ts
+++ client/src/app/card/card.component.ts
@@ -12,6 +12,11 @@ import { CardsService } from '../services/cards.service';
 export class CardComponent implements OnInit {
   card!: Card
   image!: string
+  flavor!: string
+  oracle!: string
+  power!: string
+  toughness!: string
+
   constructor(
     private ngxService: NgxUiLoaderService,
     private route: ActivatedRoute,
@@ -28,6 +33,10 @@ export class CardComponent implements OnInit {
       const id = String(this.route.snapshot.paramMap.get('id'))
       this.cardsService.getCard(id).subscribe(card => this.card = card)
       this.cardsService.getCard(id).subscribe(card => card.image_uris ? this.image = card.image_uris.border_crop : this.image = card.card_faces[0].image_uris.border_crop)
+      this.cardsService.getCard(id).subscribe(card => card.flavor_text ? this.flavor = card.flavor_text : this.flavor = card.card_faces[0].flavor_text)
+      this.cardsService.getCard(id).subscribe(card => card.oracle_text ? this.oracle = card.oracle_text : this.oracle = card.card_faces[0].oracle_text)
+      this.cardsService.getCard(id).subscribe(card => card.power ? this.power = card.power : this.power = card.card_faces[0].power)
+      this.cardsService.getCard(id).subscribe(card => card.toughness ? this.toughness = card.toughness : this.toughness = card.card_faces[0].toughness)
       this.ngxService.stop()
     }
 
@@ -46,6 +55,10 @@ export class CardComponent implements OnInit {
     this.card.card_faces.forEach(face => {
       if (faceImageUri != face.image_uris.border_crop) {
         this.image = face.image_uris.border_crop
+        this.flavor = face.flavor_text
+        this.oracle = face.oracle_text
+        this.power = face.power
+        this.toughness = face.toughness
       }
     });
   }
