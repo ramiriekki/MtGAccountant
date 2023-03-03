720cb1efee6d9110a8589907120181fcb42b60d6
ramiriekki <rami.riekkinen@gmail.com>
2023-03-03 09:53:49 +0200

    Search result sorting, Sorting by collected value not working

--- client/src/app/services/search.service.ts
+++ client/src/app/services/search.service.ts
@@ -13,9 +13,9 @@ export class SearchService {
     private httpClient: HttpClient
   ) { }
 
-  searchCards(searchData: any){
+  searchCards(searchData: any): Observable<any>{
     const data = { name: searchData.name, rarities: searchData.rarities, setTypes: searchData.setTypes, minPrice: searchData.minPrice, maxPrice: searchData.maxPrice, sets: searchData.sets, owned: searchData.owned}
-    
+
     return this.httpClient.post(this.url + `/api/search/cards`, data, {
       headers: new HttpHeaders().set('Content-Type', 'application/json')
     })
