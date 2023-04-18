8cba6df935df817aa834f901da4ffc0bc6c1e6d2
ramiriekki <rami.riekkinen@gmail.com>
2023-04-18 16:04:11 +0300

    Paginate the binder dialog

--- client/src/app/set/binder-modify/binder-modify.component.ts
+++ client/src/app/set/binder-modify/binder-modify.component.ts
@@ -8,7 +8,10 @@ import { Card } from 'src/app/models/Card';
   styleUrls: ['./binder-modify.component.css']
 })
 export class BinderModifyComponent implements OnInit {
-  testData: Card[] = []
+  leftCards: Card[] = []
+  rightCards: Card[] = []
+  leftIndexes: {start: number, end: number} = {start: 0, end: 9}
+  rightIndexes: {start: number, end: number} = {start: 9, end: 18}
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: Card[]) { }
 
@@ -18,9 +21,35 @@ export class BinderModifyComponent implements OnInit {
   }
 
   ngAfterViewInit(): void {
-    this.testData = this.data.slice(0, 9);
-    console.log(this.testData);
+    this.leftCards = this.data.slice(this.leftIndexes.start, this.leftIndexes.end);
+    this.rightCards = this.data.slice(this.rightIndexes.start, this.rightIndexes.end);
+  }
 
+  handleNextOpening(): void {
+    if (this.rightCards[this.rightCards.length - 1].id !== this.data[this.data.length - 1].id) {
+      this.leftIndexes.start = this.leftIndexes.start + 18
+      this.leftIndexes.end = this.leftIndexes.end + 18
+
+      this.rightIndexes.start = this.rightIndexes.start + 18
+      this.rightIndexes.end = this.rightIndexes.end + 18
+
+      this.leftCards = this.data.slice(this.leftIndexes.start, this.leftIndexes.end);
+      this.rightCards = this.data.slice(this.rightIndexes.start, this.rightIndexes.end);
+    }
+
+  }
+
+  handlePreviousOpening(): void {
+    if (this.leftIndexes.start != 0) {
+      this.leftIndexes.start = this.leftIndexes.start - 18
+      this.leftIndexes.end = this.leftIndexes.end - 18
+
+      this.rightIndexes.start = this.rightIndexes.start - 18
+      this.rightIndexes.end = this.rightIndexes.end - 18
+
+      this.leftCards = this.data.slice(this.leftIndexes.start, this.leftIndexes.end);
+      this.rightCards = this.data.slice(this.rightIndexes.start, this.rightIndexes.end);
+    }
   }
 
 }
