1fa1300ed7ba58cb9af573acdc78b6f97c185884
ramiriekki <rami.riekkinen@gmail.com>
2023-05-26 22:56:11 +0300

    Basic tooltips

--- client/src/defaults/tooltipDefaults.ts
+++ client/src/defaults/tooltipDefaults.ts
@@ -2,7 +2,7 @@ import { MatTooltipDefaultOptions } from "@angular/material/tooltip";
 
 export const tooltipDefaults: MatTooltipDefaultOptions = {
   showDelay: 600,
-  hideDelay: 1000,
+  hideDelay: 500,
   position: "below",
   touchendHideDelay: 0
 };
