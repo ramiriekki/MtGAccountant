f254cd97f31e3e6db69699ad1d5a463c2030172d
ramiriekki <rami.riekkinen@gmail.com>
2023-07-06 15:46:05 +0300

    Able to GET images from the local folder

--- server/src/main/java/com/github/mtgaccountant/server/restImplementation/ImageRestImpl.java
+++ server/src/main/java/com/github/mtgaccountant/server/restImplementation/ImageRestImpl.java
@@ -1,8 +1,14 @@
 package com.github.mtgaccountant.server.restImplementation;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.pdfbox.io.IOUtils;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +32,16 @@ public class ImageRestImpl implements ImageRest {
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.MULTI_STATUS);
         }
+    }
+
+    @Override
+    public ResponseEntity<byte[]> getImage(String id) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        InputStream in = new FileInputStream(
+                "/Users/ramiriekkinen/Projects/School/MtGAccountant/server/data/images/angular_architecture.png");
+        byte[] media = IOUtils.toByteArray(in);
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+        return new ResponseEntity<byte[]>(media, headers, HttpStatus.OK);
     }
 
 }
