e7c18d77a000c01623f63a7928c5e4b260a60b0f
ramiriekki <rami.riekkinen@gmail.com>
2023-07-13 12:14:53 +0300

    Dashboard refactor and unsubscribe

--- client/src/app/dashboard/top-card/top-card.component.ts
+++ client/src/app/dashboard/top-card/top-card.component.ts
@@ -1,56 +1,76 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Subject, takeUntil } from 'rxjs';
 import { Card } from 'src/app/models/Card';
 import { CardsService } from 'src/app/services/cards.service';
 import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
-  selector: 'app-top-card',
-  templateUrl: './top-card.component.html',
-  styleUrls: ['./top-card.component.css']
+    selector: 'app-top-card',
+    templateUrl: './top-card.component.html',
+    styleUrls: ['./top-card.component.css'],
 })
-export class TopCardComponent implements OnInit {
-  mostValuableCard!: Card;
-  collectionValue: number = 0;
-  totalCollectedAmount: number = 0;
-  @Input()
-  user: any;
+export class TopCardComponent implements OnInit, OnDestroy {
+    protected _unsubscribe$: Subject<void> = new Subject();
+    mostValuableCard!: Card;
+    collectionValue: number = 0;
+    totalCollectedAmount: number = 0;
+    @Input()
+    user: any;
 
-  constructor(
-    private router: Router,
-    private ngxService: NgxUiLoaderService,
-    private cardsService: CardsService,
-    private loaderService: LoaderService
-  ) { }
+    constructor(
+        private router: Router,
+        private ngxService: NgxUiLoaderService,
+        private cardsService: CardsService,
+        private loaderService: LoaderService
+    ) {}
 
-  ngOnInit(): void {
-    this.getMostValuableCard()
-    this.getCollectionValue()
-    this.getCollectionCount()
-  }
+    ngOnInit(): void {
+        this.getMostValuableCard();
+        this.getCollectionValue();
+        this.getCollectionCount();
+    }
 
-  ngAfterViewInit() {
-    this.loaderService.topCardLoaded()
- }
+    ngOnDestroy(): void {
+        this._unsubscribe$.next();
+        this._unsubscribe$.complete();
+    }
 
-  getMostValuableCard(): void {
-    this.ngxService.start()
-    this.cardsService.getMostValuableCard().subscribe(card => this.mostValuableCard = card).add(() => {
-    });
-    this.ngxService.stop()
-  }
+    ngAfterViewInit() {
+        this.loaderService.topCardLoaded();
+    }
 
-  getCollectionValue(): void {
-    this.cardsService.getCollectionValue().subscribe(value => this.collectionValue = +value.toFixed(2))
-  }
+    getMostValuableCard(): void {
+        this.ngxService.start();
+        this.cardsService
+            .getMostValuableCard()
+            .pipe(takeUntil(this._unsubscribe$))
+            .subscribe((card) => {
+                this.mostValuableCard = card;
+                this.ngxService.stop();
+            })
+            .add(() => {});
+    }
 
-  getCollectionCount(): void {
-    this.cardsService.getCollectionCount().subscribe(count => this.totalCollectedAmount = +count)
-  }
+    getCollectionValue(): void {
+        this.cardsService
+            .getCollectionValue()
+            .pipe(takeUntil(this._unsubscribe$))
+            .subscribe((value) => (this.collectionValue = +value.toFixed(2)));
+    }
 
-  moveToTopCard(): void {
-    this.router.navigate(['dashboard/collection)', this.mostValuableCard.id])
-  }
+    getCollectionCount(): void {
+        this.cardsService
+            .getCollectionCount()
+            .pipe(takeUntil(this._unsubscribe$))
+            .subscribe((count) => (this.totalCollectedAmount = +count));
+    }
 
+    moveToTopCard(): void {
+        this.router.navigate([
+            'dashboard/collection)',
+            this.mostValuableCard.id,
+        ]);
+    }
 }
